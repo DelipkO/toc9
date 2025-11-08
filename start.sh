d41a0a819c058dbc8fcc749b9de6480fc5445de5
DelipkO <novikovandreyalex@gmail.com>
2025-11-09 01:10:07 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-python test_bot.py
+set -e  # Прерывать выполнение при ошибках
+
+echo "Checking Python version..."
+python3 --version
+
+echo "Installing dependencies..."
+pip3 install -r requirements.txt
+
+echo "Starting bot..."
+exec python3 test_bot.py
